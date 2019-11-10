849fafa8e40d9c18d3dd67fdef95433321557dee
Anh Van <anhvan2505@yahoo.com>
2019-11-10 20:49:25 +0100

    Add questions test

--- app.js
+++ app.js
@@ -1,3 +1,12 @@
+var question = 1;
+var order = [1, 2, 3, 4];
+var questions = [
+	"How surprised are you to encounter an actual bike with wheels?", 
+	"How easily do you imagine an actual bike with wheels?", 
+	"How good an example of a bike do you consider an actual bike with wheels?", 
+	"How similar is an actual bike with wheels to an ideal bike?"];
+var index = 0;
+
 (function(){
  	// Your web app's Firebase configuration
   	var firebaseConfig = {
@@ -18,4 +27,40 @@
 	const dbRefObject = firebase.database().ref().child('object');
 
 	dbRefObject.on('value', snap => console.log(snap.val()));
+
+  	order = shuffle(order);
 }());
+
+var key = firebase.database().ref().push().key;
+
+function shuffle(array) {
+  var currentIndex = array.length, temporaryValue, randomIndex;
+
+  // While there remain elements to shuffle...
+  while (0 !== currentIndex) {
+
+    // Pick a remaining element...
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex -= 1;
+
+    // And swap it with the current element.
+    temporaryValue = array[currentIndex];
+    array[currentIndex] = array[randomIndex];
+    array[randomIndex] = temporaryValue;
+  }
+
+  return array;
+}
+
+function updateQuestion(){
+	document.getElementById("question").innerHTML = questions[index];
+}
+
+function answer(score) {
+	firebase.database().ref(key).update({
+	    [order[index]]: score
+	});
+	index++;
+	updateQuestion();
+}
+
